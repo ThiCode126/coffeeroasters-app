89deead9b6f18540c55b4a00c3b7ed51d821d670
ThiCode126 <59418646+ThiCode126@users.noreply.github.com>
2021-02-03 18:33:23 +0100

    Up

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { ReactSVG } from "react-svg";
 
 const Footer = () => {
   return (
     <footer id="footer">
       <div className="content-wrapper">
-        <div className="in-footer">Footer</div>
+        <div className="in-footer">
+          <ReactSVG src="assets/logo.svg" className="logo" />
+          <nav className="navbar-footer">
+            <Link className="link" to="/">
+              Home
+            </Link>
+            <Link className="link" to="/about">
+              About Us
+            </Link>
+            <Link className="link" to="/plan">
+              Create You Plan
+            </Link>
+          </nav>
+          <div className="social">
+            <ReactSVG
+              src="assets/icon-facebook.svg"
+              className="icon"
+              wrapper="span"
+            />
+            <ReactSVG
+              src="assets/icon-twitter.svg"
+              className="icon"
+              wrapper="span"
+            />
+            <ReactSVG
+              src="assets/icon-instagram.svg"
+              className="icon"
+              wrapper="span"
+            />
+          </div>
+        </div>
       </div>
     </footer>
   );
