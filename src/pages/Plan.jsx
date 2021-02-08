554cf51bd2055e48027fa700d7284419cda0021c
ThiCode126 <59418646+ThiCode126@users.noreply.github.com>
2021-02-08 15:23:15 +0100

    End Select organisation

--- src/pages/Plan.jsx
+++ src/pages/Plan.jsx
@@ -9,51 +9,81 @@ const Plan = () => {
   const { works } = data;
   const { hero, step } = dataPlan;
 
-  console.log(step);
-
   let defaultState = {};
 
   for (let i = 0; i < step.length; i++) {
-    defaultState[step[i].id] = "";
+    defaultState[step[i].id] = {
+      title: "",
+      active: false,
+      disabled: false,
+    };
   }
+  defaultState[0]["active"] = true;
 
   const [myState, setMyState] = useState(defaultState);
 
   const onValueChange = (id, title) => {
-    console.log(id, title);
+    console.log(id);
     let newState = { ...myState };
-    newState[id] = title;
+    newState[id]["title"] = title;
+
+    if (id === 0) {
+      if (title === "Capsule") {
+        newState[3]["disabled"] = true;
+      } else {
+        newState[3]["disabled"] = false;
+      }
+    }
+
+    if (id === 2 && newState[3]["disabled"] === true) {
+      newState[4]["active"] = true;
+    } else if (id <= 3) {
+      const idMore = id + 1;
+      newState[idMore]["active"] = true;
+    }
     setMyState(newState);
   };
 
   const BtnSelect = ({ data }) => {
-    console.log(data);
+    const { id: stepId, title: stepTitle, name: stepName } = data;
+    console.log(stepId);
+
     const toggleAcc = (e) => {
       e.preventDefault();
       e.target.parentElement.classList.toggle("active");
     };
+
     return (
       <>
-        <button className="accordion active" onClick={toggleAcc}>
-          <span className="title">{data.title}</span>
+        <button
+          id={stepId}
+          className={`accordion${myState[stepId]["active"] ? " active" : ""}`}
+          onClick={toggleAcc}
+          disabled={myState[stepId]["disabled"] ? true : false}
+        >
+          <span className="title">{stepTitle}</span>
         </button>
         <div className="panel">
-          {data.choice.map((current, k) => (
-            <div key={k} className="choice">
-              <input
-                type="radio"
-                name={data.name}
-                value={current.title}
-                checked={myState[data.id] === current.title}
-                onChange={() => onValueChange(data.id, current.title)}
-                id={`${data.id}_${current.title}`}
-              />
-              <label htmlFor={`${data.id}_${current.title}`}>
-                <h5 className="title">{current.title}</h5>
-                <p className="desc">{current.desc}</p>
-              </label>
-            </div>
-          ))}
+          {data.choice.map((current, k) => {
+            //const { id, name } = data;
+            const { title, desc } = current;
+            return (
+              <div key={k} className="choice">
+                <input
+                  type="radio"
+                  name={stepName}
+                  value={title}
+                  checked={myState[stepId]["title"] === title}
+                  onChange={() => onValueChange(stepId, title)}
+                  id={`${stepId}_${title}`}
+                />
+                <label htmlFor={`${stepId}_${title}`}>
+                  <h5 className="title">{title}</h5>
+                  <p className="desc">{desc}</p>
+                </label>
+              </div>
+            );
+          })}
         </div>
       </>
     );
