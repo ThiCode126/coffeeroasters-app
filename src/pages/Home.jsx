5ba6e2a3600a10fb0db419bfa9eea53c6eb5917c
ThiCode126 <59418646+ThiCode126@users.noreply.github.com>
2021-02-11 09:52:28 +0100

    Up design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { ReactSVG } from "react-svg";
+
 import Article from "../Components/Article";
 import Button from "../Components/Button";
 import CardWhy from "../Components/CardWhy";
 import CardWorks from "../Components/CardWorks";
 import Hero from "../Components/Hero";
 
+import useWindowDimensions from "../utils/useWindowDimensions";
 import data from "../utils/dataHome.json";
 
 const Home = () => {
   const { hero, collection, why, works } = data;
+  const { isTablet, isDesktop } = useWindowDimensions();
   return (
     <div id="home">
       <Hero data={hero} page="home" />
@@ -41,6 +45,20 @@ const Home = () => {
         <div className="content-wrapper">
           <div className="in-section in-works">
             <h2 className="title">{works.title}</h2>
+            {isTablet && (
+              <ReactSVG
+                src="assets/bandeau-tablet.svg"
+                className="bandeau"
+                wrapper="div"
+              />
+            )}
+            {isDesktop && (
+              <ReactSVG
+                src="assets/bandeau-desktop.svg"
+                className="bandeau"
+                wrapper="div"
+              />
+            )}
             <div className="cards">
               {works.articles.map((article, k) => (
                 <CardWorks article={article} key={k} />
--- src/utils/useWindowDimensions.js
+++ src/utils/useWindowDimensions.js
@@ -2,11 +2,26 @@ import { useState, useEffect } from 'react';
 
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
-  const isMobile = width < 768 ? true : false;
+  // const isMobile = width < 768 ? true : false;
+  // const isTablet = width >= 768 ? width < 1440 ? true : false;
+  let isMobile = false;
+  let isTablet = false;
+  let isDesktop = false;
+
+  if (width < 768) {
+    isMobile = true;
+  } else if (width >= 768 && width < 1440) {
+    isTablet = true;
+  } else if (width >= 1440) {
+    isDesktop = true;
+  }
+
   return {
     width,
     height,
-    isMobile
+    isMobile,
+    isTablet,
+    isDesktop
   };
 }
 
